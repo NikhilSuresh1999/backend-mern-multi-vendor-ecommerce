bcb0afd91662827808d4ae849e8d828d4ab7ce6e
Nikhil <nikhilkalakkara05@gmail.com>
2026-04-10 21:27:33 +0530

    Remove .env from tracking and update server configuration

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app)
 
 
 app.use(cors({
-  origin: process.env.MODE === 'pro' ? [process.env.client_customer_production_url,] : ['http://localhost:3000', 'http://localhost:3001']  ,
+  origin: process.env.MODE === 'pro' ? [process.env.client_customer_production_url,process.env.client_admin_production_url] : ['http://localhost:3000', 'http://localhost:3001']  ,
   credentials : true
 }))
 
@@ -20,7 +20,7 @@ app.use(cors({
 
 const io = socket(server,{
   cors: {
-    origin: process.env.MODE === 'pro' ? [process.env.client_customer_production_url,] : ['http://localhost:3000', 'http://localhost:3001']  ,
+    origin: process.env.MODE === 'pro' ? [process.env.client_customer_production_url,process.env.client_admin_production_url] : ['http://localhost:3000', 'http://localhost:3001']  ,
     credentials: true
   }
 })
